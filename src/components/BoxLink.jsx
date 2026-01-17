c7a5f0c2cbe9ac6fc093da89299d76a8212b0076
David <65056778+dpami507@users.noreply.github.com>
2026-01-16 22:43:09 -0500

    Reworked BoxLink Card
    now shows important info so it doesn't need to be hovered over or clicked on to get general idea of the project

--- src/components/BoxLink.jsx
+++ src/components/BoxLink.jsx
@@ -1,15 +1,33 @@
 import { getImagePath } from '../utils/helpers'
 import './BoxLink.css'
 
-function BoxLink({img, link, title, description}) {
+function BoxLink({img, link, title, description, tech}) {
+
+  const techStack = Array.isArray(tech) ? tech : tech ? tech.split(',').map(t => t.trim()) : [];
+
   return (
     <>
-        <button onClick={link} className='container'>
-          <div>
-            <h3>{title}</h3>
+      <div class="card-container" onClick={link}>
+        <div class="card-img">
+          <img src={getImagePath(img)} alt={title || "Project Screenshot"} loading="lazy"></img>
+        </div>
+        <div class="card-info">
+          <h3>{title}</h3>
+          <p>&nbsp;&nbsp;&nbsp;&nbsp;{description}</p>
+          <div class="bottom-card">
+            <div class="tech-stack">
+              <ul>
+                {techStack.map((tech, index) => (
+                  <li key={index}>{tech}</li>
+                ))}
+              </ul>
+            </div>
+            <div class="see-more">
+              <a onClick={link}>See More</a>
+            </div>
           </div>
-          <img src={getImagePath(img)} alt={title || "Project Screenshot"} loading="lazy"/>
-        </button>
+        </div>
+      </div>
     </>
   )
 }
